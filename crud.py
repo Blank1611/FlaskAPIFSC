a62c27b9496ad6be90b6c7613973d915a117d2f6
sa--sondarkar <saisondarkar18@gmail.com>
2019-05-19 13:07:10 +0530

    Updated Crud.py

--- crud.py
+++ crud.py
@@ -33,29 +33,58 @@ user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 
-#endpoints to get all the users  
+# endpoint to create new user
+@app.route("/user", methods=["POST"])
+def add_user():
+    username = request.json['username']
+    email = request.json['email']
+    
+    new_user = User(username, email)
 
-@app.route("/user",methods=["GET"])
-def get_users():
+    result = user_schema.dump(new_user)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify(result)
+
+
+# endpoint to show all users
+@app.route("/user", methods=["GET"])
+def get_user():
     all_users = User.query.all()
     result = users_schema.dump(all_users)
     return jsonify(result.data)
 
 
-    #end point to create new user 
+# endpoint to get user detail by id
+@app.route("/user/<id>", methods=["GET"])
+def user_detail(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
 
-@app.route("/user",methods=["POST"])
-def add_user():
+
+# endpoint to update user
+@app.route("/user/<id>", methods=["PUT"])
+def user_update(id):
+    user = User.query.get(id)
     username = request.json['username']
     email = request.json['email']
 
-    new_user = User(username,email)
+    user.email = email
+    user.username = username
 
-    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+# endpoint to delete user
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
     db.session.commit()
 
-    # return jsonify({"Status"= "Success","Username"= username,"Email ID"= email})
-    return jsonify("success")
+    return user_schema.jsonify(user)
 
 if __name__ == '__main__':
     app.run(debug= True)
